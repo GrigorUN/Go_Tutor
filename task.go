47bcd1bd6ead0a00fa985f81e08f142389baf6f9
amazonooo <155751051+amazonooo@users.noreply.github.com>
2024-01-24 11:43:22 +0300

    ю

--- task.go
+++ task.go
@@ -1,3 +1,74 @@
+package main
+
+import "fmt"
+
+func main() {
+	weekTempArr := [7]int{1, 2, 3, 4, 5, 6, 7}
+	workDaysSlice := weekTempArr[:5]
+	weekendSlice := weekTempArr[3:]
+	fromTuesdayToThursDaySlice := weekTempArr[1:6]
+	weekTempSlice := weekTempArr[:6]
+
+	fmt.Println(workDaysSlice, len(workDaysSlice), cap(workDaysSlice))                                        // [1 2 3 4 5] 5 7
+	fmt.Println(weekendSlice, len(weekendSlice), cap(weekendSlice))                                           // [6 7] 2 2
+	fmt.Println(fromTuesdayToThursDaySlice, len(fromTuesdayToThursDaySlice), cap(fromTuesdayToThursDaySlice)) // [2 3 4] 3 6
+	fmt.Println(weekTempSlice, len(weekTempSlice), cap(weekTempSlice))                                        // [1 2 3 4 5 6 7] 7 7
+}
+
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
+
+// func f(cnt *int) {
+// 	*cnt++
+// }
+
+// func main() {
+// 	// Получаем читателя пользовательского ввода
+// 	reader := bufio.NewReader(os.Stdin)
+// 	fmt.Println("Interaction counter")
+
+// 	cnt := 0
+// 	for {
+// 		fmt.Print("-> ")
+// 		// Считываем введённую пользователем строку. Программа ждёт, пока пользователь введёт строку
+// 		_, err := reader.ReadString('\n')
+// 		if err != nil {
+// 			panic(err)
+// 		}
+
+// 		f(&cnt)
+
+// 		fmt.Printf("User input %d lines\n", cnt)
+// 	}
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	num := 1
+// 	for i := 1; i < 101; i++ {
+// 		switch {
+// 		case num%3 == 0:
+// 			fmt.Println("Fizz")
+// 			num++
+// 		case num%5 == 0:
+// 			fmt.Println("Buzz")
+// 			num++
+// 		default:
+// 			fmt.Println(num)
+// 			num++
+// 		}
+// 	}
+
+// }
+
 // package main
 
 // import "fmt"
@@ -56,25 +127,3 @@
 // 		fmt.Println("Привет, альфа!")
 // 	}
 // }
-
-package main
-
-import "fmt"
-
-func main() {
-	num := 1
-	for i := 1; i < 101; i++ {
-		switch {
-		case num%3 == 0:
-			fmt.Println("Fizz")
-			num++
-		case num%5 == 0:
-			fmt.Println("Buzz")
-			num++
-		default:
-			fmt.Println(num)
-			num++
-		}
-	}
-
-}
