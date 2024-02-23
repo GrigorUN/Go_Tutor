eda3480737464230703990e3664c5f0c49b68be6
amazonooo <155751051+amazonooo@users.noreply.github.com>
2024-02-24 00:45:52 +0300

    tag

---
+++ struct/TagJSON.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type Person struct {
+	Name        string 		`json:"Имя"`
+	Email       string		`json:"Почта"`
+	DateOfBirth time.Time	`json:"-"`
+}
+
+func main(){
+	p := Person{
+		Name:	"Alex",
+		Email:	"a@yandex.ru",
+		DateOfBirth: time.Now(),
+	}
+
+	jsMan, err := json.Marshal(p)
+    if err != nil {
+        log.Fatalln("unable marshal to json")
+    }
+	fmt.Printf("Man %v", string(jsMan))
+}
